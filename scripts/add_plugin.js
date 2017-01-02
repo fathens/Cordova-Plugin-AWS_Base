39d7b55e785d19ec035b3262fecace817ca1c2ec
Kunio SAWATANI <fsawa@fathens.org>
2017-01-02 10:48:20 +0000

    plugin_id と gitrepo を自動認識

--- scripts/add_plugin.js
+++ scripts/add_plugin.js
@@ -3,37 +3,52 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
 
+const package_json_file = 'package.json';
+const plugin_xml_file = 'plugin.xml';
 const target_file = '../../config.xml';
-const plugin_id = process.argv[2]
-const gitrepo = process.argv[3]
 
-const variables = process.argv.slice(4).map((key) => {
+const variables = process.argv.slice(2).map((key) => {
     return {
         $: {
             name: key,
             value: process.env[key]
         }
-    }
-})
+    };
+});
+
+const gitrepo = require(package_json_file).repository.url;
+
+function read_plugin_id(callback) {
+    fs.readFile(plugin_xml_file, 'utf-8', (err, data) => {
+        if (err) throw err;
+        xml2js.parseString(data, (err, xml) => {
+            if (err) throw err;
+            callback(xml.plugin.$.id);
+        });
+    });
+}
+
 
 function modify(xml) {
-    const elem = {
-        $: {
-            name: plugin_id,
-            spec: gitrepo
-        },
-        variable: variables
-    };
-    
-    if (!xml.widget.plugin) xml.widget.plugin = [];
-    xml.widget.plugin.push(elem);
-    
-    const builder = new xml2js.Builder();
-    const modified = builder.buildObject(xml);
-    
-    fs.writeFile(target_file, modified, 'utf-8', (err) => {
-        if (err) throw err;
-        console.log("Added plugin: " + plugin_id);
+    read_plugin_id((plugin_id) => {
+        const elem = {
+            $: {
+                name: plugin_id,
+                spec: gitrepo
+            },
+            variable: variables
+        };
+        
+        if (!xml.widget.plugin) xml.widget.plugin = [];
+        xml.widget.plugin.push(elem);
+        
+        const builder = new xml2js.Builder();
+        const modified = builder.buildObject(xml);
+        
+        fs.writeFile(target_file, modified, 'utf-8', (err) => {
+            if (err) throw err;
+            console.log("Added plugin: " + plugin_id);
+        });
     });
 }
 
