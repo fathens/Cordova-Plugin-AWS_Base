e8a2db17adeb1d4cf2b15efd0953f3f5ff88a370
Kunio SAWATANI <fsawa@fathens.org>
2017-01-02 12:13:24 +0000

    JSON.parse を使用

--- scripts/add_plugin.js
+++ scripts/add_plugin.js
@@ -14,7 +14,13 @@ const variables = process.argv.slice(2).map((key) => {
     };
 });
 
-const gitrepo = require('./package.json').repository.url;
+function read_gitrepo(callback) {
+   fs.readFile('./package.json', 'utf-8', (err, data) => {
+       if (err) throw err;
+       const json = JSON.parse(data);
+       callback(json.repository.url);
+   });
+}
 
 function read_plugin_id(callback) {
     fs.readFile('./plugin.xml', 'utf-8', (err, data) => {
@@ -29,23 +35,25 @@ function read_plugin_id(callback) {
 
 function modify(xml) {
     read_plugin_id((plugin_id) => {
-        const elem = {
-            $: {
-                name: plugin_id,
-                spec: gitrepo
-            },
-            variable: variables
-        };
-        
-        if (!xml.widget.plugin) xml.widget.plugin = [];
-        xml.widget.plugin.push(elem);
-        
-        const builder = new xml2js.Builder();
-        const modified = builder.buildObject(xml);
-        
-        fs.writeFile(target_file, modified, 'utf-8', (err) => {
-            if (err) throw err;
-            console.log("Added plugin: " + plugin_id);
+        read_gitrepo((gitrepo) => {
+            const elem = {
+                $: {
+                    name: plugin_id,
+                    spec: gitrepo
+                },
+                variable: variables
+            };
+            
+            if (!xml.widget.plugin) xml.widget.plugin = [];
+            xml.widget.plugin.push(elem);
+            
+            const builder = new xml2js.Builder();
+            const modified = builder.buildObject(xml);
+            
+            fs.writeFile(target_file, modified, 'utf-8', (err) => {
+                if (err) throw err;
+                console.log("Added plugin: " + plugin_id);
+            });
         });
     });
 }
