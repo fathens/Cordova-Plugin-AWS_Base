e8ededa0f68f2daf6bb356ff12964222cc550c07
Kunio Sawatani <devel@fathens.org>
2017-01-26 16:32:07 +0900

    初期化は fork してから実行

--- platforms/ios/src/AwsBase.swift
+++ platforms/ios/src/AwsBase.swift
@@ -6,16 +6,23 @@ fileprivate func log(_ msg: String) {
 
 @objc(AwsBase)
 class AwsBase: CDVPlugin {
-    
     override func pluginInitialize() {
-        let infoDict: [String : String] = Bundle.main.infoDictionary!["CordovaAWS"] as! [String : String]
-        let regionName = infoDict["Region"]!
-        let poolId = infoDict["CognitoPool"]!
-    
-        let region = regionName.aws_regionTypeValue()
-        let provider = AWSCognitoCredentialsProvider.init(regionType: region, identityPoolId: poolId)
-        let config = AWSServiceConfiguration.init(region: region, credentialsProvider: provider)
-        AWSServiceManager.default().defaultServiceConfiguration = config
-        log("AWSServiceManager is initialized: region=\(region)")
+        fork {
+            let infoDict: [String : String] = Bundle.main.infoDictionary!["CordovaAWS"] as! [String : String]
+            let regionName = infoDict["Region"]!
+            let poolId = infoDict["CognitoPool"]!
+        
+            let region = regionName.aws_regionTypeValue()
+            let provider = AWSCognitoCredentialsProvider.init(regionType: region, identityPoolId: poolId)
+            let config = AWSServiceConfiguration.init(region: region, credentialsProvider: provider)
+            AWSServiceManager.default().defaultServiceConfiguration = config
+            log("AWSServiceManager is initialized: region=\(region)")
+        }
+    }
+
+    // MARK: - Private Utillities
+
+    fileprivate func fork(_ proc: @escaping () -> Void) {
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.utility).async(execute: proc)
     }
 }
